38b5f906540d8a247933e2dbe28d647c83033f72
Ariel Pineiro <arielsrv@gmail.com>
2021-07-07 22:08:16 -0300

    agrego mapeos para la tabla authors

--- Biblioseca.Model/Author.cs
+++ Biblioseca.Model/Author.cs
@@ -8,7 +8,8 @@ namespace Biblioseca.Model
 {
     public class Author
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }    
     }
 }
