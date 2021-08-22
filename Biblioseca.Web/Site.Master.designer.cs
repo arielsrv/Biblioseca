1150e8a10409fddc236913748226f5cb402444af
Ariel Piñeiro <ariel.pineiro@gmail.com>
2021-08-22 11:11:01 -0300

    master

--- Biblioseca.Web/Site.Master.designer.cs
+++ Biblioseca.Web/Site.Master.designer.cs
@@ -7,17 +7,19 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Biblioseca.Web {
-    
-    
-    public partial class SiteMaster {
-        
+namespace Biblioseca.Web
+{
+
+
+    public partial class SiteMaster
+    {
+
         /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify, move the field declaration from the designer file to a code-behind file.
+        /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
